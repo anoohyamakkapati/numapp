594f0b7a3a68383d5e2018138aacc54458c83608
Anoohya Makkapati <anoohyamakkapati@gmail.com>
2017-08-09 16:36:23 -0500

    Initial commit

---
+++ src/num.java
@@ -0,0 +1,9 @@
+public class num {
+
+    public static void main(String[] args) {
+        System.out.println("printing numbers from 1 to 100");
+        for (int i = 0; i <= 100; i++) {
+            System.out.println(i);
+        }
+    }
+}
